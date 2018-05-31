7fe22decaf637cf7c4ae701e0fc474a04da7f536
Unknown <maciek.gorczyca@poczta.fm>
2018-05-31 16:02:11 +0200

    dodanie nowych funkcji

--- src/Main.hs
+++ src/Main.hs
@@ -49,7 +49,13 @@ imageViewer
        file   <- menuPane      [text := "&File"]
        mclose <- menuItem file [text := "&Close\tCtrl+C", help := "Close the image", enabled := False]
        open   <- menuItem file [text := "&Open\tCtrl+O",  help := "Open an image"]
-       test   <- menuItem file [text := "&Test", help := "Test"]
+       mir180 <- menuItem file [text := "&Rotate 180", help := "Rotate 180"]
+       mir90  <- menuItem file [text := "&Rotate 90", help := "Rotate 90"]
+       mir270 <- menuItem file [text := "&Rotate 270", help := "Rotate 270"]
+       miscal <- menuItem file [text := "&Scale", help := "Scale"]
+       migray <- menuItem file [text := "&Grayscale", help := "Grayscale"]
+       mibrig <- menuItem file [text := "&Brighten", help := "Brighten"]
+       midark <- menuItem file [text := "&Darken", help := "Darken"]
        menuLine file
        quit   <- menuQuit file [help := "Quit the demo"]
 
@@ -63,7 +69,13 @@ imageViewer
        abimg      <- getDataFileName "bitmaps/wxwin16.png"
        tbarOpen   <- toolMenu tbar open  "Open"  foimg []
        tbarAbout  <- toolMenu tbar about "About" abimg []
-       tbarTest   <- toolMenu tbar test  "Test"  abimg []
+       tbarR90    <- toolMenu tbar mir90  "Rotate 90"  abimg []
+       tbarR180   <- toolMenu tbar mir180  "Rotate 180"  abimg []
+       tbarR270   <- toolMenu tbar mir270  "Rotate 270"  abimg []
+       tbarScal   <- toolMenu tbar miscal  "Scale"  abimg []
+       tbarGray   <- toolMenu tbar migray  "Grayscale"  abimg []
+       tbarBrig   <- toolMenu tbar mibrig  "Brighten"  abimg []
+       tbarDark   <- toolMenu tbar midark  "Darken"  abimg []
 
        -- create statusbar field
        status <- statusField   [text := "Welcome to the wxHaskell ImageViewer"]
@@ -75,21 +87,20 @@ imageViewer
              ,statusBar        := [status]
              ,menuBar          := [file,hlp]
              ,outerSize        := sz 400 300    -- niceness
-            --  ,on (menu about)  := infoDialog f "About ImageViewer" "This is a wxHaskell demo"
-            --  ,on (menu quit)   := close f
-            --  ,on (menu open)   := onOpen f sw vimage mclose status 
-            --  ,on (menu mclose) := onClose  sw vimage mclose status
-            --  ,on (menu test)   := onTest sw vimage
 
-             -- nice close down, but no longer necessary as bitmaps are managed automatically.
-            --  ,on closing       :~ \previous -> do{ closeImage vimage; previous }
              ]
         
        let networkDescription :: MomentIO ()
            networkDescription = mdo
               eAbout <- event0 tbarAbout command
               eOpen  <- event0 tbarOpen command 
-              eTest  <- event0 tbarTest command
+              eR90  <- event0 tbarR90 command
+              eR180  <- event0 tbarR180 command
+              eR270  <- event0 tbarR270 command
+              eScal  <- event0 tbarScal command
+              eGray  <- event0 tbarGray command
+              eBrig  <- event0 tbarBrig command
+              eDark  <- event0 tbarDark command
 
               let showAbout :: IO ()
                   showAbout = infoDialog f "About ImageViewer" "This is a wxHaskell demo"
@@ -125,24 +136,62 @@ imageViewer
                            Just fname -> openImage fname
 
               reactimate (openClick <$ eOpen)
-
-              let onTest :: IO ()
-                  onTest
+              
+              let manipulate :: (J.Image PixelRGB8 -> J.Image PixelRGB8) -> IO ()
+                  manipulate fun
                     = do mbImage <- swap vimage value Nothing
                          case mbImage of
                            Nothing -> return ()
                            Just im -> do
                              img <- convertToImageRGB8 im
-                             newImg <- convertToImage (rotate180 img)
+                             newImg <- convertToImage (fun img)
                              closeImage
                              set vimage [value := Just newImg]
                              imsize <- get newImg size
                              set sw [virtualSize := imsize]
                              repaint sw
 
-              reactimate (onTest <$ eTest)
+              let onRotate90 :: IO ()
+                  onRotate90
+                    = do manipulate rotate90
 
+              reactimate (onRotate90 <$ eR90)
 
+              let onRotate180 :: IO ()
+                  onRotate180
+                    = do manipulate rotate180
+
+              reactimate (onRotate180 <$ eR180)
+
+              let onRotate270 :: IO ()
+                  onRotate270
+                    = do manipulate rotate270
+
+              reactimate (onRotate270 <$ eR270)
+
+              let onScale :: IO ()
+                  onScale
+                    = do manipulate scale
+
+              reactimate (onScale <$ eScal)
+
+              let onGrayscale :: IO ()
+                  onGrayscale
+                    = do manipulate grayscale
+
+              reactimate (onGrayscale <$ eGray)
+
+              let onBrighten :: IO ()
+                  onBrighten
+                    = do manipulate brighten
+
+              reactimate (onBrighten <$ eBrig)
+
+              let onDarken :: IO ()
+                  onDarken
+                    = do manipulate darken
+
+              reactimate (onDarken <$ eDark)
 
        network <- compile networkDescription
        actuate network
@@ -153,52 +202,5 @@ imageViewer
                  case mbImage of
                    Nothing -> return () 
                    Just im -> drawImage dc im pointZero []
-  -- where
-  --   onOpen :: Frame a -> ScrolledWindow b -> Var (Maybe (W.Image ())) -> MenuItem c -> StatusField -> IO ()
-  --   onOpen f sw vimage mclose status
-  --     = do mbfname <- fileOpenDialog f False {- change current directory -} True "Open image" imageFiles "" ""
-  --          case mbfname of
-  --            Nothing    -> return ()
-  --            Just fname -> openImage sw vimage mclose status fname
-
-  --   onClose sw vimage mclose status
-  --     = do closeImage vimage
-  --          set mclose [enabled := False]
-  --          set sw     [virtualSize := sz 0 0]
-  --          set status [text := ""]
-  --          repaint sw
-
-  --   onTest sw vimage 
-  --     = do mbImage <- swap vimage value Nothing
-  --          case mbImage of
-  --             Nothing -> return ()
-  --             Just im -> do
-  --               img <- convertToImageRGB8 im
-  --               newImg <- convertToImage (rotate180 img)
-  --               closeImage vimage
-  --               set vimage [value := Just newImg]
-  --               imsize <- get newImg size
-  --               set sw [virtualSize := imsize]
-  --               repaint sw
-                
-
-  --   closeImage vimage
-  --     = do mbImage <- swap vimage value Nothing
-  --          case mbImage of
-  --            Nothing -> return ()
-  --            Just im -> objectDelete im
-
-  --   openImage sw vimage mclose status fname
-  --     = do -- load the new bitmap
-  --          im <- imageCreateFromFile fname  -- can fail with exception
-  --          closeImage vimage
-  --          set vimage [value := Just im]
-  --          set mclose [enabled := True]
-  --          set status [text := fname]
-  --          -- reset the scrollbars 
-  --          imsize <- get im size
-  --          set sw [virtualSize := imsize]
-  --          repaint sw
-  --      `onException` repaint sw
 
 
