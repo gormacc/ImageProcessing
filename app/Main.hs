f25074d0e935fd83db6083e0c1c4446274b56e98
Unknown <maciek.gorczyca@poczta.fm>
2018-05-31 11:37:25 +0200

    cos nie pyklo

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE RecursiveDo #-}
+
 module Main where
 
 import Control.Exception (onException)
 import Graphics.UI.WX 
+import Reactive.Banana
+import Reactive.Banana.WX
+import Reactive.Banana.Frameworks
 import Graphics.UI.WXCore as W
 import Codec.Picture as J
 
@@ -35,8 +40,10 @@ imageViewer
        vimage <- variable [value := Nothing]
 
        -- add a scrollable window widget in the frame
-       sw     <- scrolledWindow f [scrollRate := sz 10 10, on paint := onPaint vimage
-                                  ,bgcolor := white, fullRepaintOnResize := False]
+      --  sw     <- scrolledWindow f [scrollRate := sz 10 10, on paint := onPaint vimage
+      --                             ,bgcolor := white, fullRepaintOnResize := False]
+
+       sw     <- scrolledWindow f [scrollRate := sz 10 10, bgcolor := white, fullRepaintOnResize := False]
 
        -- create file menu
        file   <- menuPane      [text := "&File"]
@@ -51,12 +58,12 @@ imageViewer
        about  <- menuAbout hlp [help := "About ImageViewer"]
 
        -- create Toolbar
-       tbar   <- toolBar f []
-       foimg  <- getDataFileName "bitmaps/fileopen16.png"
-       abimg  <- getDataFileName "bitmaps/wxwin16.png"
-       _      <- toolMenu tbar open  "Open"  foimg []
-       _      <- toolMenu tbar about "About" abimg []
-       _      <- toolMenu tbar test  "Test"  abimg []
+       tbar       <- toolBar f []
+       foimg      <- getDataFileName "bitmaps/fileopen16.png"
+       abimg      <- getDataFileName "bitmaps/wxwin16.png"
+       tbarOpen   <- toolMenu tbar open  "Open"  foimg []
+       tbarAbout  <- toolMenu tbar about "About" abimg []
+       tbarTest   <- toolMenu tbar test  "Test"  abimg []
 
        -- create statusbar field
        status <- statusField   [text := "Welcome to the wxHaskell ImageViewer"]
@@ -68,65 +75,79 @@ imageViewer
              ,statusBar        := [status]
              ,menuBar          := [file,hlp]
              ,outerSize        := sz 400 300    -- niceness
-             ,on (menu about)  := infoDialog f "About ImageViewer" "This is a wxHaskell demo"
-             ,on (menu quit)   := close f
-             ,on (menu open)   := onOpen f sw vimage mclose status 
-             ,on (menu mclose) := onClose  sw vimage mclose status
-             ,on (menu test)   := onTest sw vimage
+            --  ,on (menu about)  := infoDialog f "About ImageViewer" "This is a wxHaskell demo"
+            --  ,on (menu quit)   := close f
+            --  ,on (menu open)   := onOpen f sw vimage mclose status 
+            --  ,on (menu mclose) := onClose  sw vimage mclose status
+            --  ,on (menu test)   := onTest sw vimage
 
              -- nice close down, but no longer necessary as bitmaps are managed automatically.
-             ,on closing       :~ \previous -> do{ closeImage vimage; previous }
+            --  ,on closing       :~ \previous -> do{ closeImage vimage; previous }
              ]
-  where
-    onOpen :: Frame a -> ScrolledWindow b -> Var (Maybe (W.Image ())) -> MenuItem c -> StatusField -> IO ()
-    onOpen f sw vimage mclose status
-      = do mbfname <- fileOpenDialog f False {- change current directory -} True "Open image" imageFiles "" ""
-           case mbfname of
-             Nothing    -> return ()
-             Just fname -> openImage sw vimage mclose status fname
+        
+       let networkDescription :: MomentIO ()
+           networkDescription = mdo
+              eAbout <- event0 tbarAbout command
 
-    onClose sw vimage mclose status
-      = do closeImage vimage
-           set mclose [enabled := False]
-           set sw     [virtualSize := sz 0 0]
-           set status [text := ""]
-           repaint sw
+              let showAbout :: IO ()
+                  showAbout = infoDialog f "About ImageViewer" "This is a wxHaskell demo"
 
-    onTest sw vimage 
-      = do mbImage <- swap vimage value Nothing
-           case mbImage of
-              Nothing -> return ()
-              Just im -> do
-                img <- convertToImageRGB8 im
-                newImg <- convertToImage (rotate180 img)
-                closeImage vimage
-                set vimage [value := Just newImg]
-                imsize <- get newImg size
-                set sw [virtualSize := imsize]
-                repaint sw
+              reactimate (showAbout <$ eAbout)
+
+
+
+       network <- compile networkDescription
+       actuate network
+  -- where
+  --   onOpen :: Frame a -> ScrolledWindow b -> Var (Maybe (W.Image ())) -> MenuItem c -> StatusField -> IO ()
+  --   onOpen f sw vimage mclose status
+  --     = do mbfname <- fileOpenDialog f False {- change current directory -} True "Open image" imageFiles "" ""
+  --          case mbfname of
+  --            Nothing    -> return ()
+  --            Just fname -> openImage sw vimage mclose status fname
+
+  --   onClose sw vimage mclose status
+  --     = do closeImage vimage
+  --          set mclose [enabled := False]
+  --          set sw     [virtualSize := sz 0 0]
+  --          set status [text := ""]
+  --          repaint sw
+
+  --   onTest sw vimage 
+  --     = do mbImage <- swap vimage value Nothing
+  --          case mbImage of
+  --             Nothing -> return ()
+  --             Just im -> do
+  --               img <- convertToImageRGB8 im
+  --               newImg <- convertToImage (rotate180 img)
+  --               closeImage vimage
+  --               set vimage [value := Just newImg]
+  --               imsize <- get newImg size
+  --               set sw [virtualSize := imsize]
+  --               repaint sw
                 
 
-    closeImage vimage
-      = do mbImage <- swap vimage value Nothing
-           case mbImage of
-             Nothing -> return ()
-             Just im -> objectDelete im
+  --   closeImage vimage
+  --     = do mbImage <- swap vimage value Nothing
+  --          case mbImage of
+  --            Nothing -> return ()
+  --            Just im -> objectDelete im
 
-    openImage sw vimage mclose status fname
-      = do -- load the new bitmap
-           im <- imageCreateFromFile fname  -- can fail with exception
-           closeImage vimage
-           set vimage [value := Just im]
-           set mclose [enabled := True]
-           set status [text := fname]
-           -- reset the scrollbars 
-           imsize <- get im size
-           set sw [virtualSize := imsize]
-           repaint sw
-       `onException` repaint sw
+  --   openImage sw vimage mclose status fname
+  --     = do -- load the new bitmap
+  --          im <- imageCreateFromFile fname  -- can fail with exception
+  --          closeImage vimage
+  --          set vimage [value := Just im]
+  --          set mclose [enabled := True]
+  --          set status [text := fname]
+  --          -- reset the scrollbars 
+  --          imsize <- get im size
+  --          set sw [virtualSize := imsize]
+  --          repaint sw
+  --      `onException` repaint sw
 
-    onPaint vimage dc _viewArea
-      = do mbImage <- get vimage value
-           case mbImage of
-             Nothing -> return () 
-             Just im -> drawImage dc im pointZero []
+  --   onPaint vimage dc _viewArea
+  --     = do mbImage <- get vimage value
+  --          case mbImage of
+  --            Nothing -> return () 
+  --            Just im -> drawImage dc im pointZero []
